6a37c9ecb1eaed6fff1e8fe2352f383f616d3746
Tomas Makaj <Tmakajcoding@gmail.com>
2023-04-30 12:33:25 -0400

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,31 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 
-import {HiOutlineMenuAlt4} from 'react-icons/hi'
-import {FaRegTimesCircle} from 'react-icons/fa'
+import { HiOutlineMenuAlt4 } from 'react-icons/hi'
+import { FaRegTimesCircle } from 'react-icons/fa'
 import Lucid2 from '../assets/Lucid2.PNG'
 
 
 const Navbar = () => {
-    const[click, setClick] = useState(false)
-    const handleClick = () => setClick(!click)
+  const [click, setClick] = useState(false)
+  const handleClick = () => setClick(!click)
 
 
   return (
     <div className='navbar'>
-        <div className='container'>
-            <img src={Lucid2} className='navBarLogo' />
-            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                <li><a href='/home' >Home</a></li>
-                <li><a href='/about' >About</a></li>
-                <li><a href='/contact' >Contact</a></li>
-                {/* <li><a href='/contact' >Contact</a></li> */}
-            </ul>
-            <div className = 'hamburger' onClick={handleClick}>
-            {click ? (<FaRegTimesCircle className='icon' />) : (<HiOutlineMenuAlt4 className='icon' />)}
-            </div>
-
+      <div className='container'>
+        <img src={Lucid2} className='navBarLogo' alt="contracting." />
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+          <li><a href='/home' >Home</a></li>
+          <li><a href='/about' >About</a></li>
+          <li><a href='/contact' >Contact</a></li>
+          {/* <li><a href='/contact' >Contact</a></li> */}
+        </ul>
+        <div className='hamburger' onClick={handleClick}>
+          {click ? (<FaRegTimesCircle className='icon' />) : (<HiOutlineMenuAlt4 className='icon' />)}
         </div>
+
+      </div>
     </div>
   )
 }
