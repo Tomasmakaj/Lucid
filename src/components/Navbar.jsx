ee09075280569ff1a93bb148ec8cd36b13ade693
Tomas Makaj <Tmakajcoding@gmail.com>
2023-11-12 14:33:28 -0500

    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,9 +16,9 @@ const Navbar = () => {
       <div className='container'>
         <img src={Lucid2} className='navBarLogo' alt="contracting." />
         <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-          <li><a href='/home' >Home</a></li>
-          <li><a href='/about' >About</a></li>
-          <li><a href='/contact' >Contact</a></li>
+          <li><a href='#/home' >Home</a></li>
+          <li><a href='#/about' >About</a></li>
+          <li><a href='#/contact' >Contact</a></li>
           {/* <li><a href='/contact' >Contact</a></li> */}
         </ul>
         <div className='hamburger' onClick={handleClick}>
